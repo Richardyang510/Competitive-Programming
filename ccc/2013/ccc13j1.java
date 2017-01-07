6f6bef973515a72be36d4495ddd13afb0e81c70f
Richard Yang <richyang510@gmail.com>
2017-01-06 23:49:27 -0500

    Create ccc13j1.java

---
+++ ccc/2013/ccc13j1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ccc13j1 {
+
+public static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        //System.out.println("");
+        
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int diff = b-a;
+        int c = diff+b;
+        
+        System.out.println(c);
+
+    }
+
+}
